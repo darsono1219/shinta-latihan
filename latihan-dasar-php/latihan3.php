a77b397386c54e451b11bccee93778c2e0393ef2
tianuwoe69 <tianuwoe69@gmail.com>
2024-09-27 14:47:50 +0700

    latihan1

---
+++ latihan-dasar-php/latihan3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>menabung</h2>
+    <form>
+        <label>saldo awal</label><br>
+        <input type=""><br><br>
+        <label>bunga</label><br>
+        <input type=""><br><br>
+        <label>bulan</label><br>
+        <input type=""><br><br>
+        <label>saldo akhir</label><br>
+        <input type=""><br><br>
+        <button type=""></button>
+    </form>
+</body>
+</html>
---
+++ latihan-dasar-php/latihan5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>kecepatan waktu</h2>
+    <form>
+        <label>jarak</label><br>
+        <input type=""><br><br>
+        <label>waktu</label><br>
+        <input type=""><br><br>
+        <label>kecepatan</label><br>
+        <input type=""><br><br>
+        <button type=""></button>
+    </form>
+</body>
+</html>
