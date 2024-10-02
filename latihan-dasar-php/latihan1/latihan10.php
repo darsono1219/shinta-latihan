519d1037c04d26b14290d831ee20ad89b1e4c5d7
tianuwoe69 <tianuwoe69@gmail.com>
2024-10-02 16:09:15 +0700

    menambah feature php

---
+++ latihan-dasar-php/latihan1/latihan10.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>rumus alas x tinggi</h2>
+    <form>
+        <label>alas</label><br>
+        <input type=" "><br><br>
+        <label>tinggi</label><br>
+        <input type=" "><br><br>
+        <button type=" "></button>
+    </form>
+</body>
+</html>
